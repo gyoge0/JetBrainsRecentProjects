f995b9292255692dc85147acc00eb7596b2a287f
Yogesh Thambidurai <gyogesh2015@outlook.com>
2024-03-28 16:48:48 -0400

    chore: update debug.ps1 to build the project and kill powertoys

--- debug.ps1
+++ debug.ps1
@@ -1,15 +1,23 @@
-Push-Location
-Set-Location $PSScriptRoot
-
-$ptPath = "C:\Program Files\PowerToys"
+# use this to quickly build the project and restart PowerToys
+# run from administrator VS Dev pwsh in project root
 
 $project = "JetBrainsRecentProjects"
 $fullName = "Community.PowerToys.Run.Plugin.$project"
 
+Write-Output "Starting build"
+# build the project in the background
+$build = Start-Job -ScriptBlock {
+    cd $fullName
+    msbuild /property:Platform=x64
+}
+
+Write-Output "Killing PowerToys"
+# kill existing PowerToys process
+Get-Process PowerToys -ErrorAction SilentlyContinue | Stop-Process -PassThru > $null
+
+$ptPath = "C:\Program Files\PowerToys"
 $debug = ".\$fullName\bin\x64\Debug\net8.0-windows"
-
 $dest = "$env:LOCALAPPDATA\Microsoft\PowerToys\PowerToys Run\Plugins\$project"
-
 $files = @(
     "$fullName.deps.json",
     "$fullName.dll",
@@ -17,10 +25,22 @@ $files = @(
     "Images"
 )
 
+Write-Output "Waiting for build completion"
+# kill build job if it doesn't finish in 10 seconds
+if (-not (Wait-Job -Job $build -Timeout 10)) {
+    Write-Error "Build job timed out"
+    Stop-Job -Job $build
+    return
+}
+
+Push-Location
+
+Write-Output "Copying items"
 Set-Location $debug
 mkdir $dest -Force -ErrorAction Ignore | Out-Null
 Copy-Item $files $dest -Force -Recurse
 
+Write-Output "Starting PowerToys"
 & "$ptPath\PowerToys.exe"
 
 Pop-Location

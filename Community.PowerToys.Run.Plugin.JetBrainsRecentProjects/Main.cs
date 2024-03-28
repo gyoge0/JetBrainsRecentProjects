b06328c401ff3615100f0e6aae6b9ca25ff58619
Yogesh Thambidurai <gyogesh2015@outlook.com>
2024-03-28 18:56:44 -0400

    feat: wire up toolbox to the plugin

--- Community.PowerToys.Run.Plugin.JetBrainsRecentProjects/Main.cs
+++ Community.PowerToys.Run.Plugin.JetBrainsRecentProjects/Main.cs
@@ -1,9 +1,15 @@
-﻿namespace Community.PowerToys.Run.Plugin.JetBrainsRecentProjects;
+﻿using System.Diagnostics;
+using System.IO;
+using System.Windows.Controls;
+using Microsoft.PowerToys.Settings.UI.Library;
+using Wox.Plugin.Logger;
+
+namespace Community.PowerToys.Run.Plugin.JetBrainsRecentProjects;
 
 using Wox.Plugin;
 
 // ReSharper disable once UnusedType.Global
-public class Main : IPlugin
+public class Main : IPlugin, IDelayedExecutionPlugin, ISettingProvider
 {
     // ReSharper disable once UnusedMember.Global
     // ReSharper disable once InconsistentNaming
@@ -12,6 +18,46 @@ public class Main : IPlugin
     public string Name => "JetBrains Recent Projects";
 
     public string Description => "Open recent projects from JetBrains IDEs";
+
+
+    private static string DefaultInstallLocation { get; } =
+        Path.Join(
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+            "JetBrains/Toolbox"
+        );
+
+    private JetBrainsToolbox Toolbox { get; } = new(DefaultInstallLocation);
+    private const string ToolboxInstallLocationKey = "ToolboxInstallLocation";
+
+
+    public IEnumerable<PluginAdditionalOption> AdditionalOptions =>
+    [
+        new()
+        {
+            Key = ToolboxInstallLocationKey,
+            DisplayLabel = "JetBrains Toolbox data directory",
+            PluginOptionType = PluginAdditionalOption.AdditionalOptionType.Textbox,
+            TextValue = DefaultInstallLocation,
+        }
+    ];
+
+    public void UpdateSettings(PowerLauncherPluginSettings settings)
+    {
+        PluginAdditionalOption? location = settings.AdditionalOptions
+            ?.First(x => x.Key == ToolboxInstallLocationKey);
+
+        if (location is null)
+        {
+            return;
+        }
+
+        Toolbox.InstallLocation = Path.Exists(location.TextValue)
+            ? location.TextValue
+            : DefaultInstallLocation;
+    }
+
+    public Control CreateSettingPanel() => throw new NotImplementedException();
+
 
     private PluginInitContext? _context;
 
@@ -24,19 +70,34 @@ public class Main : IPlugin
         _context = context ?? throw new ArgumentNullException(nameof(context));
     }
 
+    /// <summary>
+    /// Required to implement the <see cref="IPlugin"/> interface.
+    /// </summary>
     public List<Result> Query(Query query)
     {
-        return
-        [
-            new Result
-            {
-                QueryTextDisplay = query.Search,
-                IcoPath = null,
-                Title = "Test Title",
-                SubTitle = "Test Subtitle",
-                ToolTipData = new ToolTipData("Test", $"Test tooltip"),
-                ContextData = null,
-            },
-        ];
+        return [];
     }
+
+    public List<Result> Query(Query query, bool isFullQuery) => Toolbox
+        .LoadProjects()
+        .Select(project => new Result
+        {
+            Title = project.Name,
+            SubTitle = project.DisplayPath,
+            Action = _ =>
+            {
+                try
+                {
+                    Process.Start(project.Ide.Executable, project.Path);
+                    Log.Info($"Process started with {project.Ide.Executable} in {project.Path}", typeof(Main));
+                    return true;
+                }
+                catch
+                {
+                    Log.Warn($"Process not started with {project.Ide.Executable} in {project.Path}", typeof(Main));
+                    return false;
+                }
+            }
+        })
+        .ToList();
 }
